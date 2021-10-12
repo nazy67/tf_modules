bc1dffd84893b17ead8247d86bf58c1d06986834
nazy67 <nazykh67@gmail.com>
2021-10-12 15:48:49 -0500

    modified main.tf

--- bastion/main.tf
+++ bastion/main.tf
@@ -3,7 +3,9 @@ resource "aws_instance" "bastion_host" {
     instance_type          = var.instance_type
     subnet_id              = var.subnet_id
     vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-    
+    key_name               = var.key_name
+
+
     root_block_device {
       volume_size           = var.root_volume_size
       volume_type           = var.ebs_volume_type
