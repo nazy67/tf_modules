563c4591c4e930417fe2fd5b4b18ca8c65097f90
nazy67 <nazykh67@gmail.com>
2021-10-13 12:28:26 -0500

    updated variables.tf

--- bastion/variables.tf
+++ bastion/variables.tf
@@ -78,19 +78,19 @@ variable "sg_name" {
 variable "rule_type" {
   type = string
   description = "rule type"
-  default = null
+  default = "ingress"
 }
 
 variable "ssh_port" {
   type = number
   description = "ssh port"
-  default = null
+  default = 22
 }
 
 variable "protocol_type" {
   type = string
   description = "protocol type"
-  default = null
+  default = "tcp"
 }
 
 variable "ssh_ip_ingress" {
