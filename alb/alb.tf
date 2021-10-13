3b8d7751476194fdc041b447d5a5a1e890e71d5b
nazy67 <nazykh67@gmail.com>
2021-10-13 17:41:16 -0500

    reverted back to previous version

--- alb/alb.tf
+++ alb/alb.tf
@@ -36,26 +36,6 @@ resource "aws_lb_listener" "http_listener" {
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-# HTTPS listeners rule
-resource "aws_lb_listener" "https_listener" {
-  depends_on        = []
-  load_balancer_arn = var.app_lb_arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.my_certificate.arn
-
-  default_action {
     type             = "forward"
     target_group_arn = var.target_group_arn
   }
