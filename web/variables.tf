3382f22636775733a1fd6913be23b108150365eb
nazy67 <nazykh67@gmail.com>
2021-10-13 17:08:33 -0500

    modified min max size

--- web/variables.tf
+++ web/variables.tf
@@ -144,13 +144,13 @@ variable "asg_name" {
 variable "max_size" {
   type        = number
   description = "minimum size of the autoscaling group"
-  default     = null
+  default     = ""
 }
 
 variable "min_size" {
   type        = number
   description = "maximum size of the autoscaling group"
-  default     = null
+  default     = ""
 }
 
 variable "desired_capacity" {
