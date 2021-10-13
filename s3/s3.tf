d5d97901b7deedd5da79bb6c5d77ebbc819678ef
nazy67 <nazykh67@gmail.com>
2021-10-13 02:03:57 -0500

    added s3 module

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,19 +1,14 @@
 resource "aws_s3_bucket" "main" {
-  bucket = "${var.env}-tf-nazy-bucket"
-  acl    = "private"
+  bucket = var.bucket_name
+  acl    = var.is_acl
 
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${var.env}_s3_tfstate"
-    }
-  )
-}
+  tags = var.tag_name
+  }
 
 resource "aws_s3_bucket_public_access_block" "main" {
-  bucket = aws_s3_bucket.main.id
-  ignore_public_acls = var.env == "dev" ? true : false
-  restrict_public_buckets = var.env == "dev" ? true : false
-  block_public_acls   = var.env == "dev" ? true : false
-  block_public_policy = var.env == "dev" ? true : false
+  bucket                  = var.main_bucket_id
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.lock_public_policy
 }
--- s3/variables.tf
+++ s3/variables.tf
@@ -1,24 +1,72 @@
-# Providers variables
-variable "aws_region" {
-  type        = string
-  description = " aws region to deploy infra"
+variable "main_bucket_id" {
+  type = string
+  description = "main bucket id"
+  default = ""
+}
+
+variable "ignore_public_acls" {
+  type = bool
+  description = "ignore public acls"
+  default = ""
+}
+
+variable "restrict_public_buckets" {
+  type = bool
+  description = "restrict public buckets"
+  default = ""
+}
+
+variable "block_public_acls" {
+  type = bool
+  description = "block public acls"
+  default = ""
+}
+
+variable "lock_public_policy" {
+  type = bool
+  description = "lock public policy"
+  default = ""
+}
+
+variable "bucket_name" {
+  type = string
+  description = "bucket name"
+  default = ""
+}
+
+variable "is_acl" {
+  type = string
+  description = "access control list"
+  default = ""
 }
 
 # Tag variables
 variable "env" {
   type = string
+  description = "environment"
+  default = ""
 }
+
+variable "tag_name" {
+  type = string
+  description = "tag name"
+  default = ""
+}
+
 variable "managed_by" {
   type        = string
-  description = "managed by tr or cfn"
+  description = "managed by tf"
+  default = ""
 }
 
 variable "owner" {
   type        = string
   description = "owner of the resources"
+  default = ""
 }
 
 variable "giturl" {
   type        = string
   description = "git url to the template"
+  default = ""
 }
