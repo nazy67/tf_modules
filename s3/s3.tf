c2d463251a1b076b00b2afc76db51e1a48aeaac0
nazy67 <nazykh67@gmail.com>
2021-10-13 09:56:58 -0500

    fixed syntax error

--- s3/s3.tf
+++ s3/s3.tf
@@ -2,6 +2,33 @@ resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
   acl    = var.is_acl
 
+  # # Lifecycle rule
+  # lifecycle_rule {
+  #   id      = "log"
+  #   prefix = "log/"
+  #   enabled = true
+
+  #   tags = {
+  #     rule      = "log"
+  #     autoclean = "true"
+  #   }
+
+  #   expiration {
+  #     days = 90
+  #   }
+  # }
+
+  # lifecycle_rule {
+  #   id      = "images"
+  #   prefix  = "images/"
+  #   enabled = true
+
+  #   transition {
+  #     days          = 90
+  #     storage_class = "GLACIER"
+  #   }
+  # }
+
   tags = { 
     Name = var.bucket_name
     common_tags = {
@@ -18,5 +45,5 @@ resource "aws_s3_bucket_public_access_block" "main" {
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
   block_public_acls       = var.block_public_acls
-  block_public_policy     = var.lock_public_policy
+  block_public_policy     = var.block_public_policy
 }
