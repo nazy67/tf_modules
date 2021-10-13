b69d67a14c545a7ba64b5d9a374307366f394059
nazy67 <nazykh67@gmail.com>
2021-10-12 22:37:58 -0500

    modified variables.tf

--- alb/variables.tf
+++ alb/variables.tf
@@ -5,6 +5,12 @@ variable "lb_name" {
   default = null
 }
 
+variable "app_lb_arn" {
+  type = string
+  description = "load balancer arn"
+  default = null
+}
+
 variable "is_internal" {
   type = bool
   description = "internal/internet facing"
@@ -17,9 +23,90 @@ variable "load_balancer_type" {
   default = null
 }
 
+# ALB security group 
 variable "security_groups" {
   type = string
   description = "application load balance sg"
+  default = null
+}
+
+variable "lb_sg_name" {
+  type = string
+  description = "load balancer sg name"
+  default = null
+}
+variable "rule_type" {
+  type = string
+  description = "rule type"
+  default = null
+}
+
+variable "ssh_port" {
+  type = number
+  description = "ssh port"
+  default = null
+}
+
+variable "http_port" {
+  type = number
+  description = "http port"
+  default = null
+}
+
+variable "cidr_blocks" {
+  type = number
+  description = "cidr blocks"
+  default = null
+}
+
+variable "protocol_type" {
+  type = string
+  description = "protocol type"
+  default = null
+}
+
+variable "launch_template_sg" {
+  type = string
+  description = "launch template sg"
+  default = null
+}
+
+variable "lb_sg_description" {
+  type = number
+  description = "load balancer sg description"
+  default = null
+}
+
+# VPC id
+variable "vpc_id" {
+  type = string
+  description = "vpc id"
+  default = null
+}
+
+# HTTP listeners rule
+variable "app_lb_arn" {
+  type = string
+  description = "application load balancer arn"
+  default = null
+}  
+
+variable "action_type" {
+  type = string
+  description = "action type forward/redirect"
+  default = null
+}
+
+# Targer group variables
+variable "target_group_name" {
+  type = string
+  description = "target group name"
+  default = null
+}
+
+variable "target_group_arn" {
+  type = string
+  description = "target group arn"
   default = null
 }
 
