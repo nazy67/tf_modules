692d4c9c7be278a814606d87c1276ed3a57eb837
nazy67 <nazykh67@gmail.com>
2021-10-12 23:05:27 -0500

    fixed target  gpr name

--- alb/alb.tf
+++ alb/alb.tf
@@ -16,7 +16,7 @@ resource "aws_lb" "web_lb" {
 
 # Target group
 resource "aws_lb_target_group" "web_tg" {
-  name     = var.tg_name
+  name     = var.target_group_name
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
